8d5d10213d1946808b20481c490deac15fc3a579
Matt Nadareski <mnadareski@outlook.com>
2023-09-11 11:26:50 -0400

    Port code from BOS

---
+++ ObjectIdentifier.dot.cs
@@ -0,0 +1,30 @@
+namespace SabreTools.ASN1
+{
+#pragma warning disable IDE0011
+
+    /// <summary>
+    /// Methods related to Object Identifiers (OID) and dot notation
+    /// </summary>
+    public static partial class ObjectIdentifier
+    {
+        /// <summary>
+        /// Parse an OID in separated-value notation into dot notation
+        /// </summary>
+        /// <param name="values">List of values to check against</param>
+        /// <returns>List of values representing the dot notation</returns>
+#if NET48
+        public static string ParseOIDToDotNotation(ulong[] values)
+#else
+        public static string? ParseOIDToDotNotation(ulong[]? values)
+#endif
+        {
+            // If we have an invalid set of values, we can't do anything
+            if (values == null || values.Length == 0)
+                return null;
+
+            return string.Join(".", values);
+        }
+    }
+
+#pragma warning restore IDE0011
+}
