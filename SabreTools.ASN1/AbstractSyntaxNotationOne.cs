c22f0f2c6ecfdcce58f7c236967701f44dc31e95
Matt Nadareski <mnadareski@outlook.com>
2024-11-26 12:55:58 -0500

    Update static ASN1 and add tests

--- SabreTools.ASN1/AbstractSyntaxNotationOne.cs
+++ SabreTools.ASN1/AbstractSyntaxNotationOne.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace SabreTools.ASN1
 {
@@ -12,16 +14,38 @@ namespace SabreTools.ASN1
         /// </summary>
         /// <param name="data">Byte array representing the data</param>
         /// <param name="pointer">Current pointer into the data</param>
-        /// <returns></returns>
         public static List<TypeLengthValue> Parse(byte[] data, int pointer)
         {
+            // If the data is invalid
+            if (data.Length == 0)
+                throw new InvalidDataException(nameof(data));
+            if (pointer < 0 || pointer >= data.Length)
+                throw new IndexOutOfRangeException(nameof(pointer));
+
+            using var stream = new MemoryStream(data);
+            stream.Seek(pointer, SeekOrigin.Begin);
+            return Parse(stream);
+        }
+
+        /// <summary>
+        /// Parse a stream into a DER-encoded ASN.1 structure
+        /// </summary>
+        /// <param name="data">Stream representing the data</param>
+        public static List<TypeLengthValue> Parse(Stream data)
+        {
+            // If the data is invalid
+            if (data.Length == 0 || !data.CanRead)
+                throw new InvalidDataException(nameof(data));
+            if (data.Position < 0 || data.Position >= data.Length)
+                throw new IndexOutOfRangeException(nameof(data));
+
             // Create the output list to return
             var topLevelValues = new List<TypeLengthValue>();
 
             // Loop through the data and return all top-level values
-            while (pointer < data.Length)
+            while (data.Position < data.Length)
             {
-                var topLevelValue = new TypeLengthValue(data, ref pointer);
+                var topLevelValue = new TypeLengthValue(data);
                 topLevelValues.Add(topLevelValue);
             }
 
